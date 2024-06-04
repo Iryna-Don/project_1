b7172fb5ba178d03b4fa406c2c68c2897c920ab7
Iryna-Don <idonets0104081518@gmail.com>
2024-06-04 17:54:54 +0200

    have done the lesson 12

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+const Header = () => {
+  return (
+    <header className='header'>
+      <img
+        src='https://img.icons8.com/?size=80&id=PWMoLR7WpGdB&format=png'
+        alt='logo palm on the island'
+      />
+    </header>
+  );
+};
+export default Header;
